eb2fa3cfef0cc6d8609f52745514fdeaf5b48014
jeoniltae <foodchain@naver.com>
2026-02-05 22:00:13 +0900

    update: next.js 설치 및 페이지 설정

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+  );
+};
+
+export default NotFound;
