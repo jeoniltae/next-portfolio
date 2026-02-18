9cc239ccb927f02038a54a7476468ac30dee5615
jeoniltae <foodchain@naver.com>
2026-02-18 13:03:18 +0900

    update: dotenv 설치 및 데이터 fetching 작업

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { DATABASE_ID, TOKEN } from "@/config";
 
-const Projects = () => {
-  return <div>projects</div>;
+const Projects = async () => {
+  const data = await fetch(
+    `https://api.notion.com/v1/data_sources/${DATABASE_ID}/query`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Notion-Version": "2025-09-03",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sorts: [
+          {
+            property: "이름",
+            direction: "descending",
+          },
+        ],
+      }),
+      cache: "force-cache",
+    }
+  );
+
+  const projectData = await data.json();
+
+  const projects = projectData.results.map((v) => {
+    return v.properties.이름.title[0].plain_text;
+  });
+
+  // console.log(projects);
+  return (
+    <div>
+      <p>총 프로젝트: {projectData.results.length}</p>
+      {projectData.results.map((v) => {
+        return <h1 key={v.id}>{v.properties.이름.title[0].plain_text}</h1>;
+      })}
+    </div>
+  );
 };
 
 export default Projects;
